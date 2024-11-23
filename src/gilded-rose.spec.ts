a154866ce327867e130f5524a71df2a4b24f1f73
barbajoe <sabe12070@gmail.com>
2024-11-23 13:15:22 -0600

    made spec notes

--- src/gilded-rose.spec.ts
+++ src/gilded-rose.spec.ts
@@ -8,4 +8,23 @@ describe("Gilded Rose", () => {
 		const items = gildedRose.updateQuality();
 		expect(items[0].name).toBe("fixme");
 	});
+
+	// shared specs
+	// after sell by date (`sellIn` < 0), quality degrades 2x
+	// quality >= 0 ALWAYS (never negative)
+	// quality <= 50 ALWAYS (except Sulfuras)
+
+	// unique specs
+
+	// aged brie increases in quality as it ages (UNSURE HOW TO HANDLE)
+	// Sulfuras is unique. quality is 80 always. is legendary. has no `sellIn`
+
+	// backstage passes increase in quality until `SellIn` arrives IS UNIQUE
+	// - quality += 3, if `SellIn` <= 5
+	// - quality += 2, else if `SellIn` <= 10
+	// - quality = 0, if `SellIn` <= 0
+
+	// NEW ITEMS specs
+
+	// conjured items degrade in quality twice as fast as normal items
 });
